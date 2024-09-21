9d16958134510b3cb62541911d709365e6a495ba
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-09-21 12:29:54 +0600

    oop

--- ostad/index.js
+++ ostad/index.js
@@ -1,6 +1,6 @@
     // Object creating with Object instance
 
-    let person =new Object();
+ /*   let person =new Object();
     person.first_name = 'Jahirul';
     person.last_name = 'Islam';
     person.age = 32;
@@ -11,4 +11,35 @@
         return ` My Name is ${person.first_name} ${person.last_name}`
     }
 
-    console.log(person.addTwo())
+    console.log(person.addTwo()) */
+
+    // creating class (_BluePrint of an Object)
+
+/*    class Person  {
+        first_name = 'Jahirul'
+        last_name = 'Islam'
+        age = 32
+        phone = 1773427087
+        isBangladeshi = true
+        city = "Dhaka"
+        getName(){
+            return `My Name is ${this.first_name} ${this.last_name}`
+        }
+    }
+
+    const Person1 = new Person()
+    console.log(Person1)
+*/
+    // class constructor
+    /*
+    class Person {
+        constructor() {
+            // Constructor is a default method
+            // Constructor method executed automatically when object created
+            // Constructor method can work like others method by parameter
+            // constructor method can't return any result
+            console.log('I am a constructor')
+        }
+    }
+    const Person1 = new Person()
+*/
